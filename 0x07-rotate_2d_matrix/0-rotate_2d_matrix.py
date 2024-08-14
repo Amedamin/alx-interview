3fe2b65ef6c162285183821fd7b6ed6fcb3b12c5
Amedamin <ahmedqryqr@gmail.com>
2024-08-14 11:32:45 -0700

    yes

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,13 +8,13 @@ def rotate_2d_matrix(MATRIX):
     """
     Rotates a given 2D MATRIX 90 degrees clockwise.
     """
-    N = len(MATRIX)
+    SIZE = len(MATRIX)
     # Rotate the MATRIX layer by layer
-    for I in range(N // 2):
-        for J in range(I, N - I - 1):
+    for LAYER in range(SIZE // 2):
+        for ELEMENT in range(LAYER, SIZE - LAYER - 1):
             # Perform the 4-way exchange
-            TEMP = MATRIX[I][J]
-            MATRIX[I][J] = MATRIX[N - J - 1][I]
-            MATRIX[N - J - 1][I] = MATRIX[N - I - 1][N - J - 1]
-            MATRIX[N - I - 1][N - J - 1] = MATRIX[J][N - I - 1]
-            MATRIX[J][N - I - 1] = TEMP
+            TEMP = MATRIX[LAYER][ELEMENT]
+            MATRIX[LAYER][ELEMENT] = MATRIX[SIZE - ELEMENT - 1][LAYER]
+            MATRIX[SIZE - ELEMENT - 1][LAYER] = MATRIX[SIZE - LAYER - 1][SIZE - ELEMENT - 1]
+            MATRIX[SIZE - LAYER - 1][SIZE - ELEMENT - 1] = MATRIX[ELEMENT][SIZE - LAYER - 1]
+            MATRIX[ELEMENT][SIZE - LAYER - 1] = TEMP
