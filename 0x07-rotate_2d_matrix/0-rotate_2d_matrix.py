44f4d78f49f94c6c6ac60ee65b3b569f0803c0b9
Amedamin <ahmedqryqr@gmail.com>
2024-08-14 11:24:55 -0700

    done

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Rotate the given N x N 2D MATRIX 90 degrees clockwise in-place.
+"""
+
+def rotate_2d_matrix(MATRIX):
+    """
+    Rotates a given 2D MATRIX 90 degrees clockwise.
+    """
+    N = len(MATRIX)
+    # Rotate the MATRIX layer by layer
+    for I in range(N // 2):
+        for J in range(I, N - I - 1):
+            # Perform the 4-way exchange
+            TEMP = MATRIX[I][J]
+            MATRIX[I][J] = MATRIX[N - J - 1][I]
+            MATRIX[N - J - 1][I] = MATRIX[N - I - 1][N - J - 1]
+            MATRIX[N - I - 1][N - J - 1] = MATRIX[J][N - I - 1]
+            MATRIX[J][N - I - 1] = TEMP
